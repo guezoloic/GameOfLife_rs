f09f0efb7b729e69c0caab8f03fbb1916b40b3d3
Loïc GUEZO <loicguezo@gmail.com>
2025-05-01 09:29:47 +0200

    feat: add display function
    This commit introduces a new `display` function:
    - It takes an immutable reference to a grid of bool values
    - It takes mutable reference to a String buffer.
    
    it writes to the buffer, `X` or `.` depending on the grid's bool element.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+const WIDTH: usize = 30;
+const HEIGHT: usize = 30;
+
+fn display(grid: &[[bool; WIDTH]; HEIGHT], buffer: &mut String) {
+    buffer.clear();
+
+    for row in grid {
+        for &cell in row {
+            buffer.push(if cell { 'X' } else { '.' });
+            buffer.push(' ');
+        }
+        buffer.push('\n');
+    }
+
+    println!("{}", buffer);
+}
+
 fn main() {
-    println!("Hello, world!");
+    let grid: [[bool; WIDTH]; HEIGHT] = [[false; WIDTH]; HEIGHT];
+    let mut buffer: String = String::new();
+    
+    display(&grid, &mut buffer);
 }
